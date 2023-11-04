e8acecd72ad2fb2b41726d90048e08b7a0a13693
Yug Gupta <70101980+yugg98@users.noreply.github.com>
2023-11-04 17:14:52 +0530

    done

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,14 +1,22 @@
 const newsDb = require("../models/news.model");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const eventsDb = require("../models/events.model");
+const podcastsDb = require("../models/podcastReccomendation.model");
+const productdb = require("../models/product.model");
+
 
 exports.getHome = catchAsyncErrors(async (req, res, next) => {
     const news = await newsDb.find({}).sort({createdAt:-1}).limit(8);
     const events = await eventsDb.find({}).sort({createdAt:-1}).limit(8);
+    const podcasts = await podcastsDb.find({}).sort({createdAt:-1}).limit(8);
+    const product = await productdb.find({}).sort({createdAt:-1}).limit(8);
+
     res.status(200).json({
         success: true,
         message: "Delivered Successfully",
         news,
-        events
+        events,
+        podcasts,
+        product
       });
 })
